606a8b2ae75c2b5798fafba8d5d2f0b2c3cd4688
Ben Brook <benbrook@me.com>
2020-09-25 22:10:36 -0400

    Webhooks handling + enrichment

--- javascript/index.js
+++ javascript/index.js
@@ -1,26 +1,28 @@
-/* https://transcend-example.onrender.com/new-dsr */
+/*
+ * Developer note: this server is live at https://transcend-example.onrender.com for the eShopIt demo
+ * https://e-shop-it.trsnd.co
+ */
 
 // Libraries
 const express = require('express');
 const bodyParser = require('body-parser');
-const got = require('got');
-const jwt = require('jsonwebtoken');
-
-// Utils
-const {
-  checkIfSuspicious,
-  checkForLegalHolds
-} = require('./util');
 
 // Load environment variables
 require('dotenv').config();
+
+// Utils
 const {
-  TRANSCEND_API_KEY
-} = process.env;
+  verifyAndExtractWebhook,
+  checkIfFraudster,
+  checkForLegalHold,
+  scheduleAccessRequest,
+  scheduleErasureRequest,
+  createEnricherJwt,
+} = require('./util');
 
 // Set up the server
 const app = express();
-const port = 8080;
+const port = 8081;
 
 // Middlewares
 app.use(
@@ -33,75 +35,107 @@ app.use(bodyParser.json());
 
 app.all('/health', (_, res) => res.sendStatus(200));
 
-// Global to cache the Transcend public key.
-let cachedPublicKey;
+//////////////////////
+// WEBHOOK HANDLERS //
+//////////////////////
 
-// Receive webhook (Transcend's notification to this server)
-app.post('/new-dsr', async function (req, res, next) {
-  console.log({
-    body: req.body,
-    headers: req.headers
-  });
+/*
+ * Receive webhook for identity enrichment (optional)
+ * This path is set by you in the Admin Dashboard.
+ */
+app.post('/transcend/enrichment', async (req, res, next) => {
+  // Verify the incoming webhook is coming from Transcend, and via the Sombra gateway.
+  let signedBody;
   try {
-    // Get the public key and cache it for next time.
-    if (!cachedPublicKey) {
-      const {
-        body
-      } = await got.get(
-        'https://multi-tenant.sombra.transcend.io/public-keys/sombra-general-signing-key', {
-          headers: {
-            authorization: `Bearer ${TRANSCEND_API_KEY}`,
-          },
-        },
-      );
-      cachedPublicKey = body;
-    }
-
-    // Verify webhook signature (ensures that Transcend sent the request)
-    const isVerified = jwt.verify(req.headers['x-sombra-token'], cachedPublicKey, {
-      algorithms: ['ES384'],
-    });
-
-    if (!isVerified) {
-      // Reject the request
-      return res.status(401).send('You are not Transcend!');
-    }
-
-    // Respond 200 immediately
-    res.sendStatus(200);
-
-    /* ---- Everything below here can happen async (e.g. a job is scheduled above, and the result is returned below) ---- */
-
-    // Look up personal data
-    const userProfiles = await lookUpUser(req.body.identifiers);
-
-    // Send data to Transcend (via HTTP POST with JSON body)
-    got.post('https://multi-tenant.sombra.transcend.io/v1/upload', {
-      headers: {
-        Authorization: `Bearer ${TRANSCEND_API_KEY}`,
-        'x-transcend-nonce': req.headers['x-transcend-nonce'],
-        // 'x-sombra-authorization': `Bearer ${SOMBRA}`, // TODO: TURN OFF INTERNAL_KEY ON MULTI
-      },
-      json: {
-        profiles: userProfiles,
-      },
-    });
-  } catch (err) {
-    console.log(err);
-    next(err);
+    signedBody = await verifyAndExtractWebhook(req.headers['x-sombra-token']);
+  } catch (error) {
+    // If the webhook doesn't pass verification, reject it.
+    return res.status(401).send('You are not Transcend!');
   }
+
+  const requestIdentifier = signedBody.value;
+
+  // Add new identifers
+  const signedRequestIdentifiers = {
+    email: [
+      createEnricherJwt({
+        value: 'test+enriched@transcend.io',
+      }),
+      createEnricherJwt({
+        value: 'test+access@transcend.io',
+      }),
+    ],
+    phone: [
+      createEnricherJwt({
+        countryCode: 'US',
+        number: '+18609066012'
+      }),
+    ],
+  };
+
+  // Check if we should place a hold on this request
+  const isFraudster = await checkIfFraudster(requestIdentifier);
+  const hasLegalHold = await checkForLegalHold(requestIdentifier);
+
+  // In this case, we are automatically cancelling requests from fraudsters.
+  if (isFraudster) return res.json({
+    status: 'CANCELED',
+  });
+
+  // In this case, we are putting a hold on the request so legal can review it.
+  if (hasLegalHold) return res.json({
+    status: 'ON_HOLD',
+    signedRequestIdentifiers,
+  });
+
+  // Allow the request to proceed
+  return res.json({
+    signedRequestIdentifiers,
+  });
 });
 
-// Look inside a database and return the person's user profiles
-async function lookUpUser() {
-  return [{
-    profileId: 'ben.farrell',
-    profileData: {
-      name: 'Ben Farrell',
-      score: 3.8,
-      interests: 'Privacy Tech',
-    },
-  }, ];
-}
+/*
+ * Receive webhook (Transcend's notification to this server)
+ * This path is set by you in the Admin Dashboard.
+ */
+app.post('/transcend/new-dsr', async (req, res, next) => {
+  // console.log({
+  //   body: req.body,
+  //   headers: req.headers,
+  // });
+
+  // Verify the incoming webhook is coming from Transcend, and via the Sombra gateway.
+  try {
+    await verifyAndExtractWebhook(req.headers['x-sombra-token']);
+  } catch (error) {
+    // If the webhook doesn't pass verification, reject it.
+    return res.status(401).send('You are not Transcend!');
+  }
+
+  // Extract metadata from the webhook
+  const userIdentifier = req.body.coreIdentifier; // we know this
+  const dataSubjectType = req.body.dataSubject.type; // ACCESS, ERASURE, etc: https://docs.transcend.io/docs/receiving-webhooks#events
+  const webhookType = req.type;
+
+  // Depending on the type of webhook, respond accordingly.
+  switch (webhookType) {
+    case 'ACCESS':
+      // Schedule the job to run. Results of the job are sent to Transcend separately (in a different HTTP request, in case the job is slow).
+      scheduleAccessRequest(userIdentifier, dataSubjectType);
+
+      // Respond OK - webhook received properly.
+      res.sendStatus(200);
+
+    case 'ERASURE':
+      // Schedule the job to run. Results of the job are sent to Transcend separately (in a different HTTP request, in case the job is slow).
+      scheduleErasureRequest(userIdentifier, dataSubjectType);
+
+      // Respond OK - webhook received properly.
+      res.sendStatus(200);
+
+    default:
+      res.status(400).send('This type of privacy request is unimplemented.');
+  }
+});
 
 app.listen(port, () => console.log(`Example custom data silo listening on port ${port}!`));
--- javascript/util.js
+++ javascript/util.js
@@ -1,15 +1,113 @@
+// Libraries
+const got = require('got');
+const jwt = require('jsonwebtoken');
+
+// Constants
+const {
+  TRANSCEND_API_KEY,
+  ENRICHMENT_SIGNING_KEY,
+} = process.env;
+
+// Global to cache the webhook signing public key
+let cachedPublicKey;
+// In this example, the signing key is stored as a base64-encoded env var
+const DECODED_ENRICHMENT_SIGNING_KEY = Buffer.from(ENRICHMENT_SIGNING_KEY, 'base64').toString();
+
 /**
  * Check user against fraud systems
+ * Is this user suspected of fraud?
+ *
+ * Since this is a demo, it just checks for name+fraud@example.com
  */
-module.exports.checkIfSuspicious = (email, otherIdentifiers) => {
-  const flag = email.split('+')[1];
-  return flag && ['thefraudster', 'fraud', 'fraudster'].includes(flag);
+module.exports.checkIfFraudster = async (email) => {
+  const flag = email.split('@')[0].split('+')[1];
+  return ['thefraudster', 'fraud', 'fraudster'].includes(flag);
 }
 
 /**
- * Check user against legal holds
+ * Check user against legal hold
+ * Is there a reason why we can't fulfill this request right now?
+ *
+ * Since this is a demo, it just checks for name+legalhold@example.com
  */
-module.exports.checkForLegalHolds = (email, otherIdentifiers) => {
-  const flag = email.split('+')[1];
-  return flag && ['legalhold'].includes(flag);
+module.exports.checkForLegalHold = async (email) => {
+  const flag = email.split('@')[0].split('+')[1];
+  return ['legalhold'].includes(flag);
+}
+
+/**
+ * Helper to verify incoming webhook requests
+ *
+ * Transcend developers: A design choice was made not to put webhook verification on an Express middleware. It's a nice refactor, but it can be esoteric to readers.
+ *
+ * @param {string} signedToken - the JSON Web Token asymetrically signed with ES384.
+ * @returns {Object} - the signed body
+ */
+module.exports.verifyAndExtractWebhook = async (signedToken) => {
+  // Get the public key and cache it for next time.
+  if (!cachedPublicKey) {
+    try {
+      const res = await got.get(
+        'https://multi-tenant.sombra.transcend.io/public-keys/sombra-general-signing-key', {
+          headers: {
+            authorization: `Bearer ${TRANSCEND_API_KEY}`,
+          },
+        },
+      );
+      cachedPublicKey = res.body;
+    } catch (err) {
+      console.error('Failed to get public key:', err);
+    }
+  }
+
+  // Verify webhook signature with the public key (ensures that Transcend sent the request)
+  const signedBody = jwt.verify(signedToken, cachedPublicKey, {
+    algorithms: ['ES384'],
+  });
+
+  return signedBody;
+}
+
+/**
+ * Process an access request for this user
+ */
+module.exports.scheduleAccessRequest = async (userIdentifier, dataSubjectType) => {
+  const userData = lookUpUser();
+}
+
+/**
+ * Process an access request for this user
+ */
+module.exports.scheduleErasureRequest = async (userIdentifier, dataSubjectType) => {
+  lookUpUser()
+}
+
+/**
+ * Sign an identifier with a JWT
+ * @param {Object} content
+ */
+module.exports.createEnricherJwt = (content) => jwt.sign(
+  // The content to sign
+  content,
+  // The private key for the enricher (you should have uploaded the public key)
+  DECODED_ENRICHMENT_SIGNING_KEY, {
+    algorithm: 'ES384',
+    expiresIn: '1d',
+    // organization URI from https://app.transcend.io/settings#OrganizationSettings
+    audience: 'e-shop-it',
+  }
+);
+
+/**
+ * Look inside a database and return the person's user profiles
+ */
+async function lookUpUser() {
+  return [{
+    profileId: 'ben.farrell',
+    profileData: {
+      name: 'Ben Farrell',
+      score: 3.8,
+      interests: 'Privacy Tech',
+    },
+  }, ];
 }
